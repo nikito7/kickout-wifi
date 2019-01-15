efc435fa3cfc668b6aa5acedc90b48a4e833b4d5
nikito7 <45373783+nikito7@users.noreply.github.com>
2019-01-15 18:00:54 +0000

    Create kickout.sh

---
+++ kickout.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+### kickout.sh #####
+
+signal=-85
+maclist="mac1 mac2"
+
+function kick () {
+mac=$1
+dev=$2
+echo kicked $1 with $3 at $2 | logger
+ubus call hostapd.$dev del_client "{'addr':'$mac', 'reason':5, 'deauth':false, 'ban_time':0}"
+}
+
+### kickout.sh #####
+
+function kickthenall () {
+
+devlist=$(ifconfig | grep wlan | grep -v sta | awk '{ print $1 }')
+
+###
+for mac in $maclist
+do
+####
+for dev in $devlist
+do
+stalist=""
+stalist=$(iw $dev station dump | grep Station | awk '{ print $2 }')
+#####
+for sta in $stalist
+do
+rssi=$(iw $dev station get $sta | grep "signal avg" | awk '{ print $3 }')
+if [ $sta = $mac ] && [ $rssi -lt $signal ]
+then
+kick $mac $dev $rssi
+fi
+done
+#####
+done
+####
+done
+###
+}
+
+### kickout.sh #####
+
+sleep 8; /bin/sh $0 &
+
+###
+##
+#
+

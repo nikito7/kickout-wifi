f37cff1248c20111b47e36d3c75782e5e8d6eb20
nikito7 <45373783+nikito7@users.noreply.github.com>
2019-01-15 20:23:53 +0000

    Update kickout.sh

--- kickout.sh
+++ kickout.sh
@@ -8,39 +8,40 @@ function kick () {
 mac=$1
 dev=$2
 echo kicked $1 with $3 at $2 | logger
-ubus call hostapd.$dev del_client "{'addr':'$mac', 'reason':5, 'deauth':true, 'ban_time':0}"
+ubus call hostapd.$dev \
+del_client "{'addr':'$mac', \
+'reason':5, 'deauth':false, 'ban_time':0}"
 }
 
-### kickout.sh #####
-
-devlist=$(ifconfig | grep wlan | grep -v sta | awk '{ print $1 }')
+devlist=$(ifconfig | grep wlan | \
+grep -v sta | awk '{ print $1 }')
 
 ###
-for mac in $maclist
-do
-####
 for dev in $devlist
 do
 stalist=""
-stalist=$(iw $dev station dump | grep Station | awk '{ print $2 }')
-#####
+stalist=$(iw $dev station dump | \
+grep Station | awk '{ print $2 }')
+####
+for mac in $maclist
+do
+######
 for sta in $stalist
 do
-rssi=$(iw $dev station get $sta | grep "signal avg" | awk '{ print $3 }')
+rssi=$(iw $dev station get $sta | \
+grep "signal avg" | awk '{ print $3 }')
 if [ $sta = $mac ] && [ $rssi -lt $signal ]
 then
 kick $mac $dev $rssi
 fi
 done
-#####
+######
 done
 ####
 done
 ###
 
-sleep 8
-
-/bin/sh $0 &
+sleep 8; /bin/sh $0 &
 
 ###
 ##
